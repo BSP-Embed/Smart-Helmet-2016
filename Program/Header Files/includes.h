b6f3d3d2aa352f05716178e0bc734254bd8af30e
BSP Embed <bsp.embed@gmail.com>
2018-04-23 22:16:17 +0530

    Missing file added

---
+++ Program/Header Files/includes.h
@@ -0,0 +1,36 @@
+/* universal include header file 
+*******************************
+ author			: BS Prashanth
+ date created	: 30/12/2014
+*********************************/
+
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include 	<avr/io.h>
+#include	<avr/interrupt.h>
+#include 	<avr/pgmspace.h>
+#include 	<avr/sleep.h>
+#include 	<avr/eeprom.h>
+#include	<util/delay.h>
+#include 	<string.h>
+#include	<math.h>
+
+#include	"app.h"
+#include 	"const.h"
+#include	"types.h"
+#include 	"dly.h"
+#include	"buz.h"
+#include	"led.h"
+#include	"motor.h"
+#include 	"lcd.h"
+#include 	"uart.h"
+#include	"gsm.h"
+#include	"gps.h"
+#include	"sms.h"
+#include	"sleep.h"
+#include	"rf.h"
+#include	"alcohol.h"
+
+#endif
+
